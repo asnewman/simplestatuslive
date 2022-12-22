17d69dfe79177a0ae03c0d71d21099210e3891da
asnewman <ashznewman@gmail.com>
2022-12-21 16:19:11 -0800

    add slack button

---
+++ app/projects/mutations/updateSlackWebhook.ts
@@ -0,0 +1,34 @@
+import { resolver } from "@blitzjs/rpc"
+import db from "db"
+import { z } from "zod"
+import axios from "axios";
+
+const UpdateProject = z.object({
+  id: z.number(),
+  slackCode: z.string().optional(),
+})
+
+export default resolver.pipe(
+  resolver.zod(UpdateProject),
+  resolver.authorize(),
+  async ({ id, ...data }, ctx) => {
+    if (!data.slackCode) {
+      await db.project.updateMany({ where: { id, userId: ctx.session.userId }, data: {
+        slackWebhook: ""
+      } })
+      return
+    }
+
+    const slackResponse = await axios.post("https://slack.com/api/oauth.v2.access", {
+      client_id: "4492198045090.4506805403075",
+      client_secret: process.env.SLACK_SECRET,
+      code: data.slackCode,
+      authorization_code: "incoming-webhook",
+      redirect_uri: "https://simplestat.us"
+    })
+
+    await db.project.updateMany({ where: { id, userId: ctx.session.userId }, data: {
+      slackWebhook: slackResponse["incoming_webhook"].url
+    } })
+  }
+)
--- pages/projects/[projectId]/edit.tsx
+++ pages/projects/[projectId]/edit.tsx
@@ -1,11 +1,12 @@
-import { Suspense, useState } from "react"
+import { Suspense, useState, useEffect } from "react"
 import Head from "next/head"
 import { useQuery, useMutation } from "@blitzjs/rpc"
-import { useParam } from "@blitzjs/next"
+import { useParam, useRouterQuery } from "@blitzjs/next"
 
 import Layout from "app/core/layouts/Layout"
 import getProjectSecure from "app/projects/queries/getProjectSecure"
 import updateProject from "app/projects/mutations/updateProject"
+import updateSlackWebhook from "app/projects/mutations/updateSlackWebhook"
 import getProjectDependencies from "app/project-dependencies/queries/getProjectDependencies"
 import createProjectDependency from "app/project-dependencies/mutations/createProjectDependency"
 import deleteProjectDependency from "app/project-dependencies/mutations/deleteProjectDependency"
@@ -16,8 +17,13 @@ import detachManagedDependencyToProject from "app/managed-dependencies/mutations
 import TopBanner from "app/core/components/TopBanner"
 import Link from "next/link"
 
+const slackClientId = '4492198045090.4506805403075'
+const getSlackRedirectUrl = (projectId: number) => `https://slack.com/oauth/v2/authorize?scope=incoming-webhook&user_scope=&redirect_uri=https%3A%2F%2Fsimplestat.us%2Fproject%2F${projectId}%2Fedit&client_id=${slackClientId}`;
+const addToSlackBtn = (projectId: number) => <a href={getSlackRedirectUrl(projectId)} style={{alignItems: 'center', color: '#000', backgroundColor: '#fff', border: '1px solid #ddd', borderRadius: '4px', display: 'inline-flex', fontFamily: 'Lato, sans-serif', fontSize: '14px', fontWeight: 600, height: '44px', justifyContent: 'center', textDecoration: 'none', width: '204px'}}><svg xmlns="http://www.w3.org/2000/svg" style={{height: '16px', width: '16px', marginRight: '12px'}} viewBox="0 0 122.8 122.8"><path d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z" fill="#e01e5a" /><path d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z" fill="#36c5f0" /><path d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z" fill="#2eb67d" /><path d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z" fill="#ecb22e" /></svg>Add to Slack</a>
+
 export const EditProject = () => {
   const projectId = useParam("projectId", "number") || -1
+  const query = useRouterQuery();
 
   const [project, { refetch: refetchProject }] = useQuery(
     getProjectSecure,
@@ -39,6 +45,7 @@ export const EditProject = () => {
   )
   const [allManagedDependencies] = useQuery(getAllManagedDependencies, {})
   const [updateProjectMutation] = useMutation(updateProject)
+  const [updateSlackWebhookMutation] = useMutation(updateSlackWebhook)
   const [createProjectDependencyMutation] = useMutation(createProjectDependency)
   const [deleteProjectDependencyMutation] = useMutation(deleteProjectDependency)
   const [attachManagedDependencyToProjectMutation] = useMutation(attachManagedDependencyToProject)
@@ -90,6 +97,12 @@ export const EditProject = () => {
     setBanner("managed dependency added")
   }
 
+  useEffect(function processSlackCode() {
+    if (query.code && typeof query.code === "string") {
+      updateSlackWebhookMutation({ id: projectId, slackCode: query.code ||  ""})
+    }
+  }, [])
+
   return (
     <>
       <Head>
@@ -138,12 +151,13 @@ export const EditProject = () => {
             <br />
             <div>
               slack hook...:{" "}
+              {projSlackhook === "" ? addToSlackBtn(projectId) :
               <input
                 className="tui-input"
                 type="text"
                 value={projSlackhook}
                 onChange={(e) => setProjSlackhook(e.target.value)}
-              />
+              />}
             </div>
             <br />
             <button
